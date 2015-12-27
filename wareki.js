fb0e4700d076814e24fb8fcdfd32ec20ad9fb6a0
tohashi <dots64if@gmail.com>
2015-12-28 02:52:24 +0900

    Add wareki.js

---
+++ wareki.js
@@ -0,0 +1,41 @@
+const eraDataList = [
+  {
+    jaName: '平成',
+    firstDate: '1989-01-08',
+  },
+  {
+    jaName: '昭和',
+    firstDate: '1926-12-25',
+  },
+  {
+    jaName: '大正',
+    firstDate: '1912-07-30',
+  },
+  {
+    jaName: '明治',
+    firstDate: '1868-01-25'
+  }
+];
+
+export default function(value = Date.now()) {
+  const dateObj = new Date(value);
+  const year = dateObj.getFullYear();
+  const month = dateObj.getMonth() + 1;
+  const date = dateObj.getDate();
+  let wareki = year;
+
+  for (let i = 0; i < eraDataList.length; i++) {
+    let eraData = eraDataList[i];
+    let eraFirstDateObj = new Date(eraData.firstDate);
+    if (dateObj - eraFirstDateObj >= 0) {
+      let eraYear = year - eraFirstDateObj.getFullYear() + 1;
+      if (eraYear === 1) {
+        eraYear = '元'
+      }
+      wareki = `${eraData.jaName}${eraYear}年`;
+      break;
+    }
+  }
+  return wareki;
+}
+
