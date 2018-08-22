5765fe99d009e9d438b6a4497a19253562ab2d3e
tohashi <dots64if@gmail.com>
2018-08-23 02:51:09 +0900

    Fix for lint

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,74 @@
-type Options = {
-  unit?: boolean;
-  newEraEnabled?: boolean;
-  newEraName?: string;
-};
+interface IOptions {
+  unit?: boolean
+  newEraEnabled?: boolean
+  newEraName?: string
+}
 
 const eraDataList = [
   {
     code: 'newEra',
-    name: '新元号',
-    firstDate: '2019-05-01'
+    firstDate: '2019-05-01',
+    name: '新元号'
   },
   {
     code: 'heisei',
-    name: '平成',
-    firstDate: '1989-01-08'
+    firstDate: '1989-01-08',
+    name: '平成'
   },
   {
     code: 'showa',
-    name: '昭和',
-    firstDate: '1926-12-25'
+    firstDate: '1926-12-25',
+    name: '昭和'
   },
   {
     code: 'taisho',
-    name: '大正',
-    firstDate: '1912-07-30'
+    firstDate: '1912-07-30',
+    name: '大正'
   },
   {
     code: 'meiji',
-    name: '明治',
-    firstDate: '1868-01-25'
+    firstDate: '1868-01-25',
+    name: '明治'
   }
-];
+]
 
 export default function(
   value: string | number = Date.now(),
-  opts: Options = {}
+  opts: IOptions = {}
 ): string | number {
-  const dateObj = new Date(value);
-  const year = dateObj.getFullYear();
+  const dateObj = new Date(value)
+  const year = dateObj.getFullYear()
   if (isNaN(year)) {
-    return year;
+    return year
   }
-  let wareki = `${year}`;
+  let wareki = `${year}`
 
-  for (let i = 0; i < eraDataList.length; i++) {
-    const eraData = eraDataList[i];
-    const { code, firstDate } = eraData;
-    let { name } = eraData;
+  for (const eraData of eraDataList) {
+    const { code, firstDate } = eraData
+    let { name } = eraData
     if (code === 'newEra') {
       if (!opts.newEraEnabled) {
-        continue;
+        continue
       } else if (opts.newEraName != null) {
-        name = opts.newEraName;
+        name = opts.newEraName
       }
     }
-    const eraFirstDateObj = new Date(firstDate);
+    const eraFirstDateObj = new Date(firstDate)
     if (dateObj.getTime() - eraFirstDateObj.getTime() >= 0) {
-      let eraYear = `${year - eraFirstDateObj.getFullYear() + 1}`;
+      let eraYear = `${year - eraFirstDateObj.getFullYear() + 1}`
       if (eraYear === '1') {
-        eraYear = '元';
+        eraYear = '元'
       }
-      wareki = `${name}${eraYear}`;
-      break;
+      wareki = `${name}${eraYear}`
+      break
     }
   }
   if (opts.unit) {
-    wareki += '年';
+    wareki += '年'
   }
 
   if (!isNaN(Number(wareki))) {
-    return Number(wareki);
+    return Number(wareki)
   }
-  return wareki;
+  return wareki
 }
