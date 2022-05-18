8fd839d763e8a696cd2223f53b3fa5538c09f290
tohashi <dots64if@gmail.com>
2022-05-18 21:11:49 +0900

    chore: add eslint

--- src/index.ts
+++ src/index.ts
@@ -4,31 +4,31 @@ const eraDataList = [
   {
     code: 'reiwa',
     firstDate: '2019-05-01',
-    name: '令和'
+    name: '令和',
   },
   {
     code: 'heisei',
     firstDate: '1989-01-08',
-    name: '平成'
+    name: '平成',
   },
   {
     code: 'showa',
     firstDate: '1926-12-25',
-    name: '昭和'
+    name: '昭和',
   },
   {
     code: 'taisho',
     firstDate: '1912-07-30',
-    name: '大正'
+    name: '大正',
   },
   {
     code: 'meiji',
     firstDate: '1868-01-25',
-    name: '明治'
-  }
+    name: '明治',
+  },
 ]
 
-export default function(
+export default function (
   value: string | number | undefined = Date.now(),
   opts: Options = {}
 ): string | number {
@@ -41,7 +41,7 @@ export default function(
 
   for (const eraData of eraDataList) {
     const { firstDate } = eraData
-    let { name } = eraData
+    const { name } = eraData
     const eraFirstDateObj = new Date(firstDate)
     if (dateObj.getTime() - eraFirstDateObj.getTime() >= 0) {
       let eraYear = `${year - eraFirstDateObj.getFullYear() + 1}`
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,8 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   test: {
+    coverage: {
+      reporter: ['text']
+    },
   },
 })
